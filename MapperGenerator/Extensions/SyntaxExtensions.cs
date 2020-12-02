6138dafb854d84d131cde0e362b711e1b9d92d65
robersonliou <robersonliou@outlook.com>
2020-12-02 21:21:43 +0800

    initial mapper generator

---
+++ MapperGenerator/Extensions/SyntaxExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace MapperGenerator.Extensions
+{
+    public static class SyntaxExtensions
+    {
+        public static IEnumerable<(string propertyType, string propertyName)> GetProperties(this ClassDeclarationSyntax classSyntax, SemanticModel semanticModel)
+        {
+            (string propertyType, string propertyName) GetPropertyInfo(
+                PropertyDeclarationSyntax prop, SemanticModel model)
+            {
+                var declaredSymbol = model.GetDeclaredSymbol(prop);
+                var propertyType = declaredSymbol.Type.ToString();
+                var propertyName = declaredSymbol.Name;
+
+                return (propertyType, propertyName);
+            }
+
+            var propertySyntaxes = classSyntax.SyntaxTree.GetRoot().DescendantNodes().OfType<PropertyDeclarationSyntax>();
+            return propertySyntaxes.Select(prop
+                => GetPropertyInfo(prop, semanticModel));
+        }
+    }
+}
